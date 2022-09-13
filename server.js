1523b85fdc1050724938f31e7d20e1a32622dbdd
Justin Henley <justin.a.henley@gmail.com>
2022-09-13 18:43:54 +0800

    Add demo route; add logging

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
+// PACKAGES
+// Create the basic express app
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3500;
+// Logger
+const logger = require('morgan');
 
 // MIDDLEWARE
+// Log requests
+app.use(logger('dev'));
 // Handle url-encoded form data
 app.use(express.urlencoded({ extended: false }));
-
 // JSON
 app.use(express.json());
 
 // ROUTES
 app.use('/', require('./routes/index'));
+app.use('/demo', require('./routes/demo'));
 
 // Universal 404 page
 app.all('*', (req, res) => {
-  // Set response code
-  res.status(404);
   // Send response as JSON
-  res.json({ message: '404 Not Found' });
-  // TODO an HTML version
+  res.status(404).json({ message: '404 Not Found' });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
