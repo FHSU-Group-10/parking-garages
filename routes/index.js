ed0341a64cb4c073a2870f50cd38252dd2e36e91
carrymycross7 <dlthreet7@gmail.com>
2022-09-15 22:59:22 -0500

    Adding the register page, created route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('^/$|/index(.html)?', (req, res) => {
   res.send('Welcome!');
 });
+
+router.get('^/$|/register(.html)?', (req, res) => {
+  res.sendFile(path.join(appRoot+'/public/register.html'));
+});
+
 
 module.exports = router;
