664600cc1ce0e4f70bda34e2cd3a6c376b2b209f
Justin Henley <justin.a.henley@gmail.com>
2022-10-05 01:33:00 +0800

    ibm_db version works

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,6 +1,32 @@
 const { Sequelize } = require('sequelize');
+// ibm_db
+const connectDB = () => {
 
-const connectDB = async () => {
+
+const ibmdb = require('ibm_db')
+const connStr = `DATABASE=${process.env.DB_NAME};HOSTNAME=${process.env.DB_HOST_NAME};PORT=${process.env.DB_PORT};Security=SSL;SSLServerCertificate=;PROTOCOL=TCPIP;UID=${process.env.DB_USERNAME};PWD=${process.env.DB_PASSWORD};`
+
+ibmdb.open(connStr, (err, connection) => {
+  if (err){
+    console.error(err);
+    return;
+  }
+  console.log("open!...");
+
+  connection.query('select 1 from sysibm.sysdummy1', function (err1, rows){
+    if (err1){
+      console.error(err1)
+    } else {
+      console.log(rows);
+    }
+    connection.close(function (err2) {
+      if (err2) console.error(err2)
+    })
+  })
+})
+}
+// Sequelize - Does NOT work yet
+/* const connectDB = async () => {
   try {
     console.log('Attempting to connect to DB...');
 
@@ -11,14 +37,14 @@ const connectDB = async () => {
       host: process.env.DB_HOST_NAME,
       port: process.env.DB_PORT,
       dialect: 'db2',
-      //ssl: '',
+      ssl: true,
     });
-
+    console.log('Created, about to authenticate')
     await sequelize.authenticate();
     console.log('Connected to DB.');
   } catch (error) {
     console.error('Failed to connect to DB. ', error);
   }
 };
-
+ */
 module.exports = connectDB;
