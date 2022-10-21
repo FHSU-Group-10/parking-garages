291abfbdb8a8c47fcf95ca7bfbf76db915f7cc8b
Justin Henley <justin.a.henley@gmail.com>
2022-10-21 23:12:26 +0800

    Fix to make all models use same sequelize instance
    - models/index.js automatically attaches all models to the seqeulize instance
    - config/modelRelations.js contains all relations between models (FKs)
    - config/dbConn.js creates sequelize instance
    - It then calls above two files to add all models and relations

---
+++ config/modelRelations.js
@@ -0,0 +1,74 @@
+const addRelations = (sequelize) => {
+  // Get needed models
+  // Any added models should be named here before establishing relations
+  const { Users, Reservation, ReservationStatus, ReservationType, Vehicle } =
+    sequelize.models;
+
+  try {
+    // Relationships
+
+    /* NOTE: 
+      Foreign key must be named on both sides of the association.
+      This is not normal, I don't know why it is happening. 
+      Otherwise, Sequelize will add a second foreign key with a different name, and queries will fail. 
+    */
+
+    // Foreign keys on Reservations
+    const reservationFK = {
+      memberId: {
+        foreignKey: {
+          name: 'MEMBER_ID',
+          allowNull: false,
+        },
+      },
+      reservationTypeId: {
+        foreignKey: {
+          name: 'RESERVATION_TYPE_ID',
+          allowNull: false,
+        },
+      },
+      vehicleId: {
+        foreignKey: {
+          name: 'VEHICLE_ID',
+          allowNull: true,
+        },
+      },
+      statusId: {
+        foreignKey: {
+          name: 'STATUS_ID',
+          allowNull: false,
+        },
+      },
+    };
+
+    Users.hasMany(Reservation, reservationFK.memberId);
+    Reservation.belongsTo(Users, reservationFK.memberId);
+
+    ReservationType.hasMany(Reservation, reservationFK.reservationTypeId);
+    Reservation.belongsTo(ReservationType, reservationFK.reservationTypeId);
+
+    Vehicle.hasMany(Reservation, reservationFK.vehicleId);
+    Reservation.belongsTo(Vehicle, reservationFK.vehicleId);
+
+    ReservationStatus.hasMany(Reservation, reservationFK.statusId);
+    Reservation.belongsTo(ReservationStatus, reservationFK.statusId);
+
+    // Foreign keys on Vehicle
+    const vehicleFK = {
+      memberId: {
+        foreignKey: {
+          name: 'MEMBER_ID',
+          allowNull: false,
+        },
+      },
+    };
+    Users.hasMany(Vehicle, vehicleFK.memberId);
+    Vehicle.belongsTo(Users, vehicleFK.memberId);
+
+    console.log('Relations added to models.');
+  } catch (error) {
+    console.log('Failed to add relations to models.');
+  }
+};
+
+module.exports = addRelations;
