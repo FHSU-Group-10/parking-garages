d0099a12103ca2a1d6b7d75cc525ad43be353740
Hannah Carr <hannahcarr@Hannahs-MacBook-Pro.local>
2022-10-23 15:15:46 -0400

    add garage controller

--- controllers/garageController.js
+++ controllers/garageController.js
@@ -1,12 +1,26 @@
 /**
  * Lists the garages that exist in the system
  * GET request
- *
  * @returns {[Object]} - An array of existing garages
  */
+// const _ = require("lodash");
+const Db = require('./models/garage');
+const { Sequelize, Op } = require("sequelize");
+const getModels = Db.getModels;
+const bcrypt = require("bcrypt");
 const listGarages = async (req, res) => {
+try {
+    // Find pricing for the specified garage
+    const results = await Garage.findAll();
+    // Return results
+    return res.status(200).json(results);
+  } catch (err) {
+    console.error('Garage controller failed.');
+    return res.status(500);
+  }
   // TODO
   return res.status(200).json([
+   /*
     {
       id: 'garage1',
       name: 'Parkopolis',
@@ -24,64 +38,21 @@ const listGarages = async (req, res) => {
       spotsPerFloor: [10, 20, 20, 0, 15],
       overbookRate: 1.1,
       isActive: false,
-    },
+    },*/
   ]);
 };
-
-/**
- * Adds a garage with the specified configuration
- * POST request
- *
- * @param {string} name -
- * @param {string} location -
- * @param {number} numFloors -
- * @param {[number]} spotsPerFloor -
- * @param {number} overbookRate -
- * @param {boolean} isActive -
- * @returns {Object} - Signals whether the garage was created successfully
- *
- * Preconditions:
- *  - numFloors >= 1
- *  - Every value of spotsPerFloor >= 0
- *  - overbookRate >= 100%
- * Postconditions:
- *  - A new garage is created in the system with the given characteristics
- */
-const addGarage = async (req, res) => {
-  // TODO
-  // Get arguments from POST request body
-  const name = req.body.name;
-  const location = req.body.location;
-  const numFloors = req.body.numFloors;
-  const spotsPerFloor = req.body.spotsPerFloor;
-  const overbookRate = req.body.overbookRate;
-  const isActive = req.body.isActive;
-
-  // Return early if any arguments missing
-  if (!(name && location && numFloors && spotsPerFloor && overbookRate && isActive)) {
-    return res.status(400).json({ message: 'Incomplete request.' });
+const getGarageId = async (req, res) => {
+try {
+    // Find pricing for the specified garage
+    const results = await Garage.findAll(GARAGE_ID);
+    // Return results
+    return res.status(200).json(results);
+  } catch (err) {
+    console.error('Garage controller failed.');
+    return res.status(500);
   }
-
-  // TODO check preconditions
-  if (numFloors < 1) {
-    return res.status(400).json({ message: 'Number of floors must be >= 1.' });
-  }
-  if (!spotsPerFloor.every((spots) => spots >= 0)) {
-    return res.status(400).json({ message: 'Every floor must have at least 0 spots.' });
-  }
-  if (numFloors != spotsPerFloor.length) {
-    return res.status(400).json({ message: 'Number of floors does not match length of spotsPerFloor array.' });
-  }
-  if (overbookRate < 1.0) {
-    return res.status(400).json({ message: 'Overbook rate must be at least 100%.' });
-  }
-
-  // TODO create the garage in the DB
-  const result = { message: 'Garage created.' };
-
-  // TODO signal success
-  return res.status(200).json(result);
 };
+
 
 /**
  * Updates a specified garage configuration
@@ -105,19 +76,19 @@ const addGarage = async (req, res) => {
  */
 const updateGarage = async (req, res) => {
   // TODO
-  // Get arguments from request body
-  const garageId = req.body.garageId;
-  const name = req.body.name;
-  const numFloors = req.body.numFloors;
-  const spotsPerFloor = req.body.spotsPerFloor;
-  const overbookRate = req.body.overbookRate;
-  const isActive = req.body.isActive;
+  // Get arguments from request url query
+  const garageId = req?.body?.id;
+  const garageName = req?.body?.name;
+  const floors = req?.body?.numFloors;
+  const spotsPerFloor = req?.body?.spotsPerFloor;
+  const location = req?.body?.location;
+  const overbookRate=req?.body?.overbookRate;
+  const isActive=req?.body?.isActive
 
-  // Return early if any arguments missing
-  if (!(garageId && name && numFloors && spotsPerFloor && overbookRate && isActive)) {
-    return res.status(400).json({ message: 'Incomplete request.' });
+   // Return early if any arguments are missing
+  if (!(garageName && floors && spotsPerFLoor && location && isActive)) {
+    return res.status(400).json({ message: 'Incomplete request' });
   }
-
   // Check preconditions
   if (numFloors < 1) {
     return res.status(400).json({ message: 'Number of floors must be >= 1.' });
@@ -131,20 +102,32 @@ const updateGarage = async (req, res) => {
   if (overbookRate < 1.0) {
     return res.status(400).json({ message: 'Overbook rate must be at least 100%.' });
   }
-
-  // TODO Check if garageId is valid
-  const validGarages = ['garage1', 'garage2', 'garage3'];
+     // TODO Check if garageId is valid
+  const validGarages = getGarageId();
   if (!validGarages.includes(garageId)) {
     return res.status(400).json({ message: 'garageId does not exist.' });
   }
+  try {
+    //update pricing
+    const result = await Garage.update(
+        {
+          DESCRIPTION: garageName,
+          FLOOR_COUNT: floors,
+          LAT: location[0],
+          LONG: location[1],
+          OVERBOOK_RATE: overbookRate,
+          IS_ACTIVE: isActive,
+        },
+        {where: {GARAGE_ID: garageId}}
+    )
 
-  // TODO Update the garage in the database
-  const result = { message: 'Garage updated.' };
-
-  // TODO signal success
+  // Return the result
   return res.status(200).json(result);
+  }catch (err) {
+    console.error('Garage controller failed.');
+    return res.status(500);
+  }
 };
-
 /**
  * Removes a specified garage
  * DELETE request
@@ -165,18 +148,119 @@ const deleteGarage = async (req, res) => {
   if (!garageId) {
     return res.status(400).json({ message: 'garageId is required.' });
   }
-
-  // TODO Check if garageId is valid
-  const validGarages = ['garage1', 'garage2', 'garage3'];
+//check if garage is valid
+  const validGarages = getGarageId();
   if (!validGarages.includes(garageId)) {
     return res.status(400).json({ message: 'garageId does not exist.' });
   }
 
   // TODO Delete garage from database
-  const result = { message: 'Garage deleted.' };
+try {
+  await Post.destroy({
+    where: {
+      GARAGE_ID: garageId
+    },
+    force: true
+  });
+  const result = {message: 'Garage deleted.'};
 
-  // TODO Signal the result
   return res.status(200).json(result);
+  }catch(err) {
+    console.error('Garage controller failed.');
+    return res.status(500);
+  }
 };
 
-module.exports = { listGarages, addGarage, updateGarage, deleteGarage };
+/**
+ * Adds a specified garage
+ *
+ * @param {string} name - The name of the garage to delete
+ * @param {number} numFloors  -floors in the garage
+ * @param {[number]} spotsPerFloor -number of spots on each floor
+ * @param {[string]} location - [latitude, longitude]
+ * @param {number} overbookRate - if null then 0
+ * @param {boolean} isActive - if true then garage is active
+ * @returns {Object} - Signals whether the garage was successfully created
+ *
+ * Preconditions:
+ *  - numFloors >= 1
+ *  - Every value of spotsPerFloor >= 0
+ *  - overbookRate >= 100%
+ * Postconditions:
+ *  - A new garage is created in the system with the given characteristics
+ */
+const addGarage = async (req, res) => {
+  // TODO
+  // Get arguments from request url query
+  const garageName = req?.body?.name;
+  const floors = req?.body?.numFloors;
+  const spotsPerFloor = req?.body?.spotsPerFloor;
+  const location = req?.body?.location;
+  const overbookRate=req?.body?.overbookRate;
+  const isActive=req?.body?.isActive
+
+  // Return early if any arguments are missing
+  if (!(garageName && floors && spotsPerFLoor && location && isActive)) {
+    return res.status(400).json({ message: 'Incomplete request' });
+  }
+
+  // Check preconditions
+  if (numFloors < 1) {
+    return res.status(400).json({ message: 'Number of floors must be >= 1.' });
+  }
+  if (!spotsPerFloor.every((spots) => spots >= 0)) {
+    return res.status(400).json({ message: 'Every floor must have at least 0 spots.' });
+  }
+  if (numFloors != spotsPerFloor.length) {
+    return res.status(400).json({ message: 'Number of floors does not match length of spotsPerFloor array.' });
+  }
+  if (overbookRate < 1.0) {
+    return res.status(400).json({ message: 'Overbook rate must be at least 100%.' });
+  }
+  try{
+    //not sure how this works. Do we need to set garage?
+    const [garage, created] = await Garage.findOrCreate({
+      where: {
+        DESCRIPTION: garageName,
+        FLOOR_COUNT: floors,
+        LAT: location[0],
+        LONG: location[1],
+        OVERBOOK_RATE: overbookRate,
+        IS_ACTIVE: isActive,
+      }
+    })
+
+    if (!created) {
+      return res.status(400).json({ message: "Garage Creation Failed." });
+    }
+    // Garage created in the DB
+    const result = { message: 'Garage created.' };
+
+    // Return the result
+    return res.status(200).json(result);
+  } catch (err) {
+    console.error('Garage controller failed.');
+    return res.status(500);
+  }
+};
+/**
+ * updates a specified garage
+ *
+ * @param {number} id - garageId
+ * @param {string} name - The name of the garage to delete
+ * @param {number} numFloors  -floors in the garage
+ * @param {[number]} spotsPerFloor -number of spots on each floor
+ * @param {[string]} location - [latitude, longitude]
+ * @param {number} overbookRate - if null then 0
+ * @param {boolean} isActive - if true then garage is active
+ * @returns {Object} - Signals whether the garage was successfully created
+ *
+ * Preconditions:
+ *  - Garage does exist in the database
+ * Postconditions:
+ *  - Garage is updated
+ */
+
+
+
+module.exports = { listGarages, addGarage, updateGarage, deleteGarage, getGarageId };
