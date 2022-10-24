f68e61cba0587beafde16a5f886160910642998e
carrymycross7 <dlthreet7@gmail.com>
2022-10-23 21:29:50 -0500

    added function to handle error modal

--- public/script/login.js
+++ public/script/login.js
@@ -10,6 +10,16 @@
             password: ''
         }
         
+        const error_modal = {
+            message: '',
+            status: '',
+            show: (error) => {
+                error_modal.message = error.data.error;
+                error_modal.status = error.status;
+                $('#error_modal').modal('show');
+            }
+        }
+        
         
         
         let loading = 0;
@@ -24,9 +34,13 @@
                     loading=0;
                     if (resp.data.is_operator) window.location.href = 'http://localhost:3500/view/operator'
                     console.dir(resp); // debug - remove
+                }, (reject) => {
+                    error_modal.show(reject);
+                    console.dir(reject); // debug - remove
                 }) // TODO: handle error message
         }
         return {
+            error_modal,
             loading,
             login,
             Login
