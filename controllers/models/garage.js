e1c9ea4f2e126648ce4ec3b0b0397aa095ddb9f9
Hannah Carr <hannahcarr@Hannahs-MacBook-Pro.local>
2022-10-24 08:32:53 -0400

    updated garage model to new format

--- controllers/models/garage.js
+++ controllers/models/garage.js
@@ -1,74 +1,50 @@
 // init sequelize
-const { Sequelize, DataTypes, Model } = require('sequelize');
-// ready our db connection function
-const connectDB = require("../../config/dbConn");
-const _ = require('lodash');
-
-const Model_Cache = {
-    initialized: false,
-    getSql:() => connectDB()
-}
-
-async function getModels() {
-    
-    const sequelize = await Model_Cache.getSql();
-    
-    const Base_Options = {
-        sequelize: sequelize,
-        timestamps: false,
-        schema: "YHL46872"
-    }
-
-    class Garage extends Model{}
-    Garage.init({
-        GARAGE_ID: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
+module.exports = (sequelize, DataTypes) => {
+    sequelize.define(
+        'Garage',
+        {
+            GARAGE_ID: {
+                type: DataTypes.NUMERIC,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            DESCRIPTION: {
+                type: DataTypes.STRING(128),
+                allowNull: false,
+                field: "DESCRIPTION",
+                unique: true
+            },
+            FLOOR_COUNT: {
+                type: DataTypes.NUMERIC,
+                allowNull: false,
+                field: "FLOOR_COUNT"
+            },
+            LAT: {
+                type: DataTypes.STRING(64),
+                allowNull: false,
+                field: "LAT"
+            },
+            LONG: {
+                type: DataTypes.STRING(64),
+                allowNull: false,
+                field: "LONG"
+            },
+            OVERBOOK_RATE: {
+                type: DataTypes.FLOAT,
+                allowNull: true,
+                field: "OVERBOOK_RATE"
+            },
+            IS_ACTIVE: {
+                type: DataTypes.BOOL,
+                allowNull: false,
+                field: "IS_ACTIVE"
+            }
         },
-        DESCRIPTION: {
-            type: DataTypes.STRING(128),
-            allowNull: false,
-            field: "DESCRIPTION",
-            unique: true
-        },
-        FLOOR_COUNT: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            field: "FLOOR_COUNT"
-        },
-        LAT: {
-            type: DataTypes.STRING(64),
-            allowNull: false,
-            field: "LAT"
-        },
-        LONG: {
-            type: DataTypes.STRING(64),
-            allowNull: false,
-            field: "LONG"
-        },
-        OVERBOOK_RATE: {
-            type: DataTypes.FLOAT,
-            allowNull: true,
-            field: "OVERBOOK_RATE"
-        },
-        IS_ACTIVE: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            field: "IS_ACTIVE"
+        {
+            tableName: 'GARAGE',
+            timestamps: false,
+            schema: 'YHL46872',
+            initialized: true,
         }
-    }, _.merge({}, Base_Options, {
-        tableName: 'GARAGE'
-    }));
-    
-    _.merge(Model_Cache, {
-        Garage
-    },{initialized: true});
-    
-    return Model_Cache;
-    
-}
-
-module.exports = {
-    getModels
+    );
 }
