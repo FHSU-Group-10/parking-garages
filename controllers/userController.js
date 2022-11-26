09d865d4f199c92c9ab94a9903ab95f6b2287aa4
Victor Munoz <vemunoz@mail.fhsu.edu>
2022-11-26 16:09:52 -0500

    setup registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,21 +14,21 @@ const { Sequelize, Op } = require("sequelize");
 const getModels = Db.getModels;
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
-const {token} = require("morgan");
+const { token } = require("morgan");
 const Users = Db.models.Users;
 
-const login = async(req, res) => {
+const login = async (req, res) => {
     try {
         // setup params
         let obj = {
             username: req.body?.Login?.username,
             password: req.body?.Login?.password
+        };
+
+        for (let param in obj) {
+            if (!obj[param]) return res.status(400).json({ error: `Incomplete Login attempt, ${param} is required!` });
         }
-        
-        for (let param in obj){
-            if (!obj[param]) return res.status(400).json({error:`Incomplete Login attempt, ${param} is required!`});
-        }
-        
+
         // find our user attempting to login
         let user = await Users.findOne({
             where: {
@@ -38,12 +38,12 @@ const login = async(req, res) => {
             }
         });
         // separating dataValues from return set
-        user =  (user || {}).dataValues;
-    
+        user = (user || {}).dataValues;
+
         const jwt = require('jsonwebtoken');
-        
+
         if (user) {
-            const password_valid = await bcrypt.compare(obj.password,user.PW);
+            const password_valid = await bcrypt.compare(obj.password, user.PW);
             if (password_valid) {
                 let new_token = jwt.sign({
                     "id": user.MEMBER_ID,
@@ -53,34 +53,35 @@ const login = async(req, res) => {
                     "phone": user.PHONE,
                     "username": user.USERNAME,
                     "is_operator": user.IS_OPERATOR
-                },`${process.env.JWT_SECRET_KEY}`, {expiresIn: '1h'});
+                }, `${process.env.JWT_SECRET_KEY}`, { expiresIn: '1h' });
                 return res.status(200).json(
                     {
                         token: new_token,
                         first_name: user.FIRST_NAME,
                         last_name: user.LAST_NAME,
                         username: user.USERNAME,
-                        is_operator: user.IS_OPERATOR}
-            ); // TODO: change to login token
+                        is_operator: user.IS_OPERATOR
+                    }
+                ); // TODO: change to login token
             } else {
-                return res.status(400).json({error: 'Password Incorrect'});
+                return res.status(400).json({ error: 'Password Incorrect' });
             }
         } else {
-            return res.status(404).json({error: "User does not exist."})
+            return res.status(404).json({ error: "User does not exist." });
         }
-        
-    }catch (ex) {
+
+    } catch (ex) {
         console.dir(ex); // log error
         return res.status(400).json(ex);
     }
-}
+};
 
 const test = async (req, res) => {
     return new Promise(async (resolve, reject) => {
         try {
             // fetch DB models, connect to DB
-            let {Users} = await getModels();
-        
+            let { Users } = await getModels();
+
             let [created_user, created] = await Users.findOrCreate({
                 where: {
                     USERNAME: req.body.username,
@@ -97,12 +98,12 @@ const test = async (req, res) => {
             // cleanup PW, do not want to send this back to the UI
             delete created_user.PW;
             resolve((200).json(created_user));
-        }catch (ex) {
+        } catch (ex) {
             console.dir(ex); // log error
             reject((400).json(ex));
         }
     });
-}
+};
 
 const register = async (req, res) => {
     try {
@@ -116,50 +117,55 @@ const register = async (req, res) => {
             "phone": "phone number required",
             "is_operator": "selection required"
         };
-        
+
         // check required fields
         // make sure the param is there and has a value
+        // for (let param in required_fields) {
+        //     if (!req?.body.newUser[param] && !req?.body.newUser.hasOwnProperty(param)) throw required_fields[param];
+        // }
+
+        //check required fields
+        // make sure the param is there and is not an emptry string
         for (let param in required_fields) {
-            if (!req.body[param] && !req.body.hasOwnProperty(param)) throw required_fields[param];
+            if (!req?.body.newUser[param] && req?.body.newUser[param] == '') throw required_fields[param];
         }
-    
         let existing_user = await Users.findOne({
             where: {
                 USERNAME: {
-                    [Op.like]: req.body.username
+                    [Op.like]: req.body.newUser.username
                 }
             }
         });
-    
-        if (existing_user) throw 'Username already in use.'
-    
+
+        if (existing_user) throw 'Username already in use.';
+
         const salt = await bcrypt.genSalt(10);
-        
-        const hashed_pw = await bcrypt.hash(req.body.password, salt);
+
+        const hashed_pw = await bcrypt.hash(req.body.newUser.password, salt);
 
         let created_user = await Users.create({
-               USERNAME: req.body.username,
-               PW: hashed_pw,
-               FIRST_NAME: req.body.first_name,
-               LAST_NAME: req.body.last_name,
-               EMAIL: req.body.email,
-               PHONE: req.body.phone,
-               IS_OPERATOR: req.body.is_operator || false
+            USERNAME: req.body.newUser.username,
+            PW: hashed_pw,
+            FIRST_NAME: req.body.newUser.first_name,
+            LAST_NAME: req.body.newUser.last_name,
+            EMAIL: req.body.newUser.email,
+            PHONE: req.body.newUser.phone,
+            IS_OPERATOR: req.body.is_operator || false
         });
         // separating dataValues from return set
         created_user = (created_user || {}).dataValues;
-    
+
         // delete PW since we do not want to send this back to the UI.
         delete created_user.PW;
-        
-       return res.status(200).json(created_user);
-    }catch (ex) {
+
+        return res.status(200).json(created_user);
+    } catch (ex) {
         console.dir(ex); // log error
         return res.status(400).json(ex);
     }
-}
+};
 
 module.exports = {
     login,
     register
-}
+};
--- public/script/register.js
+++ public/script/register.js
@@ -1,1 +1,75 @@
-console.log('hello'); // debug - remove
+(function (window) {
+    function pageCtrl($scope, $http, $document, $timeout) {
+
+        const URLS = {
+            register: '/user/register'
+        };
+
+        const newUser = {
+            username: '',
+            password: '',
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone: '',
+            is_operator: false
+        };
+
+
+        const error_modal = {
+            message: '',
+            status: '',
+            close: () => {
+                $('.modal').modal('hide');
+            },
+            show: (error) => {
+                error_modal.message = error.data.error;
+                error_modal.status = error.status;
+                $('#error-modal').modal('show');
+            }
+        };
+
+        const loading_modal = {
+            hide: () => {
+                $('#loading-modal').modal('hide');
+
+            },
+            show: () => {
+                $('#loading-modal').modal('show');
+            }
+        };
+
+
+
+        let loading = 0;
+
+        function submitNewUser() {
+            console.log("inside submitNewUser", newUser);
+            loading_modal.show(); // show our loading icon
+            $http.post(URLS.register, { newUser })
+                .then((resp) => {
+                    loading_modal.hide();
+                }, (reject) => {
+                    error_modal.show(reject);
+                });
+
+            loading_modal.hide();
+        }
+        return {
+            error_modal,
+            loading,
+            newUser,
+            submitNewUser,
+        };
+    }
+
+
+    const app = angular.module('pageApp', []);
+
+    app.controller("pageCtrl", [
+        '$scope',
+        '$http',
+        '$document',
+        '$timeout',
+        pageCtrl]);
+})(window);
